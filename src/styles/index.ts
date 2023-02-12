88f408f463eaeecfccbbbbb5349ba3944aa65945
Matheus Rian <souzamatheusrian@gmail.com>
2023-02-11 21:57:07 -0300

    feat: config stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,18 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  createTheme,
+  styled,
+  getCssText,
+  globalCss,
+  keyframes,
+  theme,
+  css,
+} = createStitches({
+  theme: {
+    colors: {
+      gray: '#222'
+    }
+  }
+})
